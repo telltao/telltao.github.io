8692cd76ac0f690eeaf9d65e185de6e16cfca933
telltao <telltao@qq.com>
2021-06-17 20:04:42 +0800

    优化

--- start.sh
+++ start.sh
@@ -1,6 +1,18 @@
+#!/bin/sh
 # 安装jekyll
   # sudo su ;jekyll gem install jekyll
 #安装 bundle gem install bundler
 # see: https://blog.csdn.net/chen_changying/article/details/88093559
-
-bundle exec jekyll serve --host=0.0.0.0 --incremental &
+rm -rf ./_site/
+NAME=$1
+echo $NAME
+ID=`lsof -i tcp:4000`
+echo $ID
+echo "...start killed..."
+for id in $ID
+do
+kill -9 $id
+echo "...killed $id"
+done
+echo "...stop killed..."
+bundle exec jekyll serve --host=0.0.0.0 --incremental
